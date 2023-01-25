48ff8f73e69b2bcc765b1f0e5b3355776f1525f5
RafaRM <rafaelmoraes.analyst@gmail.com>
2023-01-25 17:43:47 -0300

    First commit

---
+++ src/test/java/com/example/gerenciarpessoas/api/GerenciarPessoasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gerenciarpessoas.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciarPessoasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
